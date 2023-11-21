8563aa558334adec2b9451c4597b68f84971b1ba
Angel Talero <angelgotalero@outlook.com>
2023-11-21 09:03:47 -0500

    Added eureka

--- ProductsMicroservice/Program.cs
+++ ProductsMicroservice/Program.cs
@@ -1,13 +1,17 @@
+using Arquisoft.Remote;
 using ProductsMicroservice.Services;
 using ProductsMicroservice.Services.Implementation;
+using Steeltoe.Discovery.Client;
+using Steeltoe.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 
-// Add GRPC services
+// Add system services
 builder.Services.AddGrpc();
+builder.Services.AddDiscoveryClient(builder.Configuration);
 
 // Add scoped services
 builder.Services.AddScoped<IDatabaseService, DatabaseService>();
@@ -25,6 +29,10 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
+
+app.Map("/info", () =>
+	new StringType { Value = "Hello from ProductsMicroservice!" }
+);
 
 app.MapControllers();
 app.Run();
